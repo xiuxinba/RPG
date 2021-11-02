3d52fae423dc1416db403527119b3348eda3d3f1
baw9492 <1585518874@qq.com>
2021-11-02 20:50:38 +0800

    Update BattleSystem.h

--- BattleSystem.h
+++ BattleSystem.h
@@ -6,6 +6,15 @@ using namespace std;
 
 struct BattleSystem
 {
+    //全体角色数组
+    Role allRole[8];
+
+    //我方战斗行为
+    void friBattleAction();
+
+    //敌方战斗行为
+    void EneBattleAction();
+
     //构造函数, 传入双方阵型
     BattleSystem();
 };
